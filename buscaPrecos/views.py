ed5d23fd92b2f2cf658e2b1c53363f7883dffaff
Johnny <joaogbdesouza@gmail.com>
2021-07-10 18:15:38 -0300

    Adaptando para realizar chamadas apenas quando houver consulta

--- buscaPrecos/views.py
+++ buscaPrecos/views.py
@@ -11,33 +11,43 @@ import pandas as pd
 from .models import Produto
 
 def index(request):
-    driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-
-    driver.get('https://www.casasbahia.com.br/geladeira/b')
-
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    time.sleep(5)  
-    results = driver.find_elements_by_xpath("//*[@id='__next']/div[2]/div/div/div[4]/div[2]/div/article/ul")
+    search = ''
     # create empty array to store data
     data = []
-    # loop over results
-    for result in results:
-        products = result.find_elements_by_tag_name("a")
-        for product in products:
-            print(product.find_element_by_css_selector("div[class^='ProductPrice']").text)
-            productInfo = {
-                'link': product.get_attribute('href'),
-                'imagem': product.find_element_by_tag_name('img').get_attribute('src'),
-                'nome': product.find_element_by_tag_name('p').text,
-                'preco': product.find_element_by_css_selector("div[class^='ProductPrice']").text
-            }
+    try:
+        search = request.GET['s']
+    except:
+        #do varios nada
+        print('não há consulta')
+    if(search != ''):
+        driver = webdriver.Chrome()
+        search = request.GET['s']
+        print(f"https://www.casasbahia.com.br/{search}/b")
+        driver.get(f"https://www.casasbahia.com.br/{search}/b")
 
-            data.append(productInfo)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        time.sleep(5)  
+        results = driver.find_elements_by_xpath("//*[@id='__next']/div[2]/div/div/div[4]/div[2]/div/article/ul")
+        # loop over results
+        for result in results:
+            products = result.find_elements_by_tag_name("a")
+            for product in products:
+                try:
+                    productInfo = {
+                        'link': product.get_attribute('href'),
+                        'imagem': product.find_element_by_tag_name('img').get_attribute('src'),
+                        'nome': product.find_element_by_tag_name('p').text,
+                        'preco': product.find_element_by_css_selector("div[class^='ProductPrice']").text
+                    }
+                except:
+                    print('não foi possível adicionar o produto na lista')
+
+                data.append(productInfo)
+        
+        driver.quit()
 
     context = {
         'products': data
     }
-
-    driver.quit()
     
     return render(request, 'precos\index.html', context=context)
